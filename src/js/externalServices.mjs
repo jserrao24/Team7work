a748f16c32ad957b2fb85f0bc9695d10bf23809b
Jsantos20201 <72623931+Jsantos20201@users.noreply.github.com>
2023-10-26 21:13:15 -0600

    team activity w07

--- src/js/externalServices.mjs
+++ src/js/externalServices.mjs
@@ -1,5 +1,4 @@
 const baseURL = import.meta.env.VITE_SERVER_URL;
-
 function convertToJson(res) {
   if (res.ok) {
     return res.json();
@@ -8,15 +7,25 @@ function convertToJson(res) {
   }
 }
 
-export async function getData(category = "tents") {
+export async function getProductsByCategory(category) {
   const response = await fetch(baseURL + `products/search/${category}`);
   const data = await convertToJson(response);
   return data.Result;
-    
 }
 
 export async function findProductById(id) {
   const response = await fetch(baseURL + `product/${id}`);
   const product = await convertToJson(response);
   return product.Result;
-};
+}
+
+export async function checkout(payload) {
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  };
+  return await fetch(baseURL + "checkout/", options).then(convertToJson);
+}
